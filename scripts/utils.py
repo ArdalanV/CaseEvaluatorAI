e384f208c9ce04a298a3676d21f5bdb55dda1cca
Ardalan Valadkhani <ardalanv@berkeley.edu>
2025-04-06 13:09:28 -0700

    Updated utils to have baseline intake form case

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,29 @@
 """
 Utility functions for the project are defined here
 """
+
+class Intake_Form():
+
+    def __init__(self, is_injured, is_insured, liabilities, injuries, story):
+        self.injured = is_injured
+        self.insured = is_insured
+        self.liabilities = liabilities
+        self.injuries = injuries
+        self.story = story
+
+    def is_injured(self):
+        return self.injured
+    
+    def is_insured(self):
+        return self.insured
+    
+    def get_liabilities(self):
+        return self.liabilities
+    
+    def get_injuries(self):
+        return self.injuries
+    
+    def get_story(self):
+        return self.story
+
+    
