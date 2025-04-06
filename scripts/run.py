e0af4b3f93c496ff59c40828d09b77fb6da50db2
Ardalan Valadkhani <ardalanv@berkeley.edu>
2025-04-06 12:43:26 -0700

    Starting point

---
+++ scripts/run.py
@@ -0,0 +1,14 @@
+"""
+Main script for running preproccessing and matching of the case
+"""
+
+import intake_form
+
+def main():
+    """
+    manage the program
+    """
+    
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/utils.py
@@ -0,0 +1,3 @@
+"""
+Utility functions for the project are defined here
+"""
