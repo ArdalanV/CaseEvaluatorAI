99c444ecfc672ae1740bf704dd82130023d976d3
Ardalan Valadkhani <ardalanv@berkeley.edu>
2025-04-06 14:40:13 -0700

    added case object and updated run.py

--- scripts/run.py
+++ scripts/run.py
@@ -4,8 +4,24 @@ Main script for running preproccessing and matching of the case
 import utils
 
 def process_intake_form(intake_form: utils.Intake_Form):
+    """
+    Main function for processing the form. Uses LLM to take form, put it into a
+    summarized description of the case. Create case object, and then start the matching
+    """
     processed_intake_form = intake_form
     return processed_intake_form
+
+def match_case_with_firm(case: utils.Case):
+    """
+    Function to match a case object with a law firm
+    
+    Parameters:
+        case (Case): case object defined in utils to be matched with law firm
+
+    Returns:
+        firm (Firm): firm object that will get the case referral
+    """
+    return 
 
 
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -61,6 +61,33 @@ class Intake_Form():
     def get_story(self):
         return self.story
     
+class Case():
+
+    def __init__(self, first_name: str, last_name: str, email: str, phone_num: str,
+                  case_description: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_num = phone_num
+        self.case_description = case_description
+
+    def get_first_name(self):
+        return self.first_name
+    
+    def get_last_name(self):
+        return self.last_name
+    
+    def get_email(self):
+        return self.email
+    
+    def get_phone_num(self):
+        return self.phone_num
+    
+    def get_case_description(self):
+        return self.case_description
+    
+
+    
 def make_intake_form(form: IntakeFormRequest):
     """
     A function for processing the intake form that the user filled out on the 
