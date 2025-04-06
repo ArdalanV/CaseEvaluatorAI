c54e61bb836bf792298db0c6c96f2a592e4f8311
Ardalan Valadkhani <ardalanv@berkeley.edu>
2025-04-06 16:06:27 -0700

    Lots of updates

--- scripts/run.py
+++ scripts/run.py
@@ -2,14 +2,78 @@
 Main script for running preproccessing and matching of the case
 """
 import utils
+import os
+import openai
+import requests
+
+#Loads desired prompt from prompts directory
+def load_prompt(prompt: str):
+    """
+    Reads a prompt file from the prompts directory.
+
+    Parameters:
+        prompt : name of the .txt file to be used as a prompt from prompts directory
+
+    Returns:
+        str : the desired prompt to be used for the LLM call
+    """
+    #try reading the desired prompt
+    try:
+        with open(f"prompts/{prompt}.txt", "r", encoding="utf-8") as file:
+            return file.read()
+    #otherwise return False and kill the program
+    except FileNotFoundError:
+        print("Prompt was not found, please use valid prompt")
+
 
 def process_intake_form(intake_form: utils.Intake_Form):
     """
     Main function for processing the form. Uses LLM to take form, put it into a
     summarized description of the case. Create case object, and then start the matching
     """
-    processed_intake_form = intake_form
-    return processed_intake_form
+    #Load prompt in
+    prompt_template = load_prompt("prompt1")
+    prompt = prompt_template.format(
+        accident_type = intake_form.get_accident_type()
+        involved_people = involved_people
+        injured = injured
+        injury_types = injury_types
+        sought_medical_care = sought_medical_care
+        filed_police_report = filed_police_report
+        insured = insured
+        witnesses = witnesses
+        incident_date = incident_date
+        incident_description = incident_description
+        affordability_concerns = affordability_concerns
+        location = location
+        medical_costs = medical_costs
+        )
+    #Give Ollama the required data and prompt for it's output
+    response = requests.post( "http://localhost:11434/api/generate",
+        json={
+            "model": "llama3.2",
+            "prompt": prompt,
+            "stream": False,
+            "temperature": 0
+        })
+    return 
+
+
+#Makes the local LLM call 
+def summarize_intake_with_LLM(prompt: str, intake_form: utils.Intake_Form):
+    try: 
+        response = requests.post( "http://localhost:11434/api/generate",
+            json={
+                "model": "llama3.2",
+                "prompt": prompt,
+                "stream": False,
+                "temperature": 0
+            })
+        return response.json(["response"])
+    except Exception as e:
+        print(f"Request Error from Local Llama model: {e}")
+    
+
 
 def match_case_with_firm(case: utils.Case):
     """
@@ -19,7 +83,7 @@ def match_case_with_firm(case: utils.Case):
         case (Case): case object defined in utils to be matched with law firm
 
     Returns:
-        firm (Firm): firm object that will get the case referral
+        firm (LawFirm): firm object that will get the case referral
     """
     return 
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -7,59 +7,122 @@ from datetime import date
 
 class IntakeFormRequest(BaseModel):
     name: str
-    age: int
-    injured: bool
-    insured: bool
-    liabilities:str
-    injuries: str
-    injury_type: str
+    email: str
+    phone: str
+    accident_type: str
+    involved_people: str
+    injured: str
+    injury_types: str
+    sought_medical_care: str
+    filed_police_report: str
+    insured: str
+    witnesses: str
     incident_date: date
-    location: str
-    story: str
+    affordability_costs: str
+    city: str
+    state: str
+    country: str
+    medical_costs: str
+    incident_description: str
 
 class Intake_Form():
 
-    def __init__(self, name, age, injured,insured, liabilities, injuries, injury_type, incident_date, location, story):
-        self.name = name
-        self.age = age
-        self.injured = injured
-        self.insured = insured
-        self.liabilities = liabilities
-        self.injuries = injuries
-        self.injury_type = injury_type
-        self.incident_date = incident_date
-        self.location = location
-        self.story = story
+    def __init__(self, name: str, email: str, phone: str, accident_type: str, involved_people: str, 
+                 injured: str,injury_types: str, sought_medical_care: str, filed_police_report: str,
+                 insured: str, witnesses: str, incident_date: date, incident_description: str,
+                 affordability_concerns: str, city: str, state: str, country: str,
+                 medical_costs=None):
 
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.accident_type = accident_type
+        self.involved_people = involved_people
+        self.injured = injured
+        self.injury_types = injury_types
+        self.sought_medical_care = sought_medical_care
+        self.filed_police_report = filed_police_report
+        self.insured = insured 
+        self.witnesses = witnesses
+        self.incident_date = incident_date
+        self.affordability_concerns = affordability_concerns
+        self.city = city
+        self.state = state
+        self.country = country
+        self.medical_costs = medical_costs
+        self.incident_description = incident_description
+
+    @property
     def get_name(self):
         return self.name
-    
-    def get_age(self):
-        return self.age
 
+    @property
+    def get_email(self):
+        return self.email
+    
+    @property
+    def get_phone(self):
+        return self.phone
+    
+    @property
+    def get_accident_type(self):
+        return self.accident_type
+    
+    @property
+    def get_involved_people(self):
+        return self.involved_people
+    
+    @property
     def is_injured(self):
         return self.injured
     
+    @property
+    def get_injury_types(self):
+        return self.injury_types
+
+    @property
+    def has_sought_medical_care(self):
+        return self.sought_medical_care
+
+    @property
+    def has_filed_police_report(self):
+        return self.filed_police_report
+
+    @property
     def is_insured(self):
         return self.insured
-    
-    def get_liabilities(self):
-        return self.liabilities
-    
-    def get_injuries(self):
-        return self.injuries
-    
-    def get_injury_type(self):
-        return self.injury_type
 
+    @property
+    def has_witnesses(self):
+        return self.witnesses 
+
+    @property
     def get_incident_date(self):
         return self.incident_date
+
+    @property
+    def get_incident_description(self):
+        return self.incident_description
+
+    @property
+    def has_affordability_concerns(self):
+        return self.affordability_concerns
     
-    def get_location(self):
-        return self.location
+    @property
+    def get_city(self):
+        return self.city
     
-    def get_story(self):
-        return self.story
+    @property
+    def get_state(self):
+        return self.state
+    
+    @property
+    def get_country(self):
+        return self.country
+
+    @property
+    def get_medical_costs(self):
+        return self.medical_costs
 
 #Many fields to be added or changed
 class Case():
@@ -90,9 +153,10 @@ class Case():
 #Many more fields to be added later
 class LawFirm():
 
-    def __init__(self, firm_name: str, location: str, size: str):
+    def __init__(self, email: str, firm_name: str, locations: list, size: str):
+        self.email = email
         self.firm_name = firm_name
-        self.location = location
+        self.locations = locations
         self.size = size
 
     def get_firm_name(self):
@@ -103,9 +167,6 @@ class LawFirm():
     
     def get_size(self):
         return self.size
-
-    
-
     
 def make_intake_form(form: IntakeFormRequest):
     """
